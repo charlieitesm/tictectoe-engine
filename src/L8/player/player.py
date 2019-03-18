349413981f690a541dbe860816bcc2f305bc2c70
Carlos Hdz <47310270+charlieitesm@users.noreply.github.com>
2019-03-18 14:49:21 -0600

    Add a generate_name abstract method to Player

--- src/L8/player/player.py
+++ src/L8/player/player.py
@@ -6,8 +6,12 @@ from L8.board.board import Board
 class Player(ABC):
 
     def __init__(self):
-        self.name = ""
+        self.name = self.generate_name()
 
     @abstractmethod
     def make_move(self, board: Board) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate_name(self) -> str:
         raise NotImplementedError
