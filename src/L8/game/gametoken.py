412ce9baaab49e1d7c483686821f8eb7dcf0a3b5
Carlos Hdz <47310270+charlieitesm@users.noreply.github.com>
2019-03-18 17:34:54 -0600

    Change GameToken to be a regular class instead of an abstract class

--- src/L8/game/gametoken.py
+++ src/L8/game/gametoken.py
@@ -1,11 +1,8 @@
-from abc import ABC
 
-
-class GameToken(ABC):
+class GameToken:
 
     def __init__(self, token_symbol: str):
         self.token_symbol = token_symbol
 
     def __str__(self):
         return self.token_symbol
-
